4fc2f76ae86e2c34bbfa2e091bd0d08d583e8f98
shixiuhai <15256728901@163.com>
2024-06-23 12:29:56 +0800

    添加了playwright接口请求的初始化构建

---
+++ gerapyPlaywright/demoRequests.py
@@ -0,0 +1,51 @@
+"""_summary_
+这个是基于playwright的接口封装的动态渲染测试
+Returns:
+    _type_: _description_
+"""
+import requests
+from urllib.parse import urlparse
+def selfRequests(url:str,
+                 headers:str=None,
+                 proxies:dict=None,
+                 cookies:dict=None,
+                 playwright_host:str="127.0.0.1:9001",
+                 browser_type:str="chromium",
+                 timeout:int=30)->str:
+    request_url = "http://" + playwright_host + "/rendered_by_playwright/requests"
+    json={
+        "url": f"{url}",
+        "is_block_image":True,
+        "browser_type": browser_type,
+        "timeout": timeout,
+        "return_type": "text"
+    }
+    if headers is not None:
+        json["user_agent"]=headers['User-Agent']
+    if proxies is not None:
+        if "http" in proxies:
+            json["proxy"]=proxies["http"]
+            
+    if cookies is not None:
+        cookies_json = []
+        parsed_url = urlparse(url)
+        domain = parsed_url.netloc
+        for name, value in cookies.items():
+            cookie_dict = {
+                'domain': domain,
+                'name': name,
+                'path': '/',
+                'value': value,
+            }
+            cookies_json.append(cookie_dict)
+                
+        json["cookies"]=cookies_json
+    
+    result = requests.post(url=request_url,json=json).json()
+    if result.get("code")==200:
+        return result.get("text")
+if __name__ == "__main__":
+    print(selfRequests(url="https://www.xiaohongshu.com/explore/654ed925000000001b00c2bc"))
+    
+    
+    
---
+++ scrapySpiderRedis/spiders/baiduSpiderByPlaywright.py
@@ -0,0 +1,34 @@
+from typing import Iterable
+import scrapy
+from scrapy import Request
+# from scrapy_redis.spiders import RedisSpider
+from scrapySpiderRedis.items import ScrapyspiderredisItem
+from scrapySpiderRedis.log import Logging
+from gerapyPlaywright.request import PlaywrightRequest
+
+class baiduSpiderByPlaywright(scrapy.Spider):
+    name = "baiduSpiderByPlaywright"
+    allowed_domains = ["baidu.com","bing.com"]
+    # start_urls = ["https://www.baidu.com","https://www.bing.com"]
+    start_url="https://www.baidu.com"
+    logger = Logging("baiduSpiderByPlaywright.log").get_logger() # 使用自定义日志器
+    
+    # def __init__(self, *args, **kwargs):
+    #     super(BaiduspiderSpider, self).__init__(*args, **kwargs)
+    #     # 定义实例变量 logger
+    #     self.logger = Logging("baiduSpider.log").get_logger()
+    def start_requests(self) -> Iterable[Request]:
+        yield PlaywrightRequest(url=self.start_url,callback=self.parse)
+
+    def parse(self, response):
+        self.logger.info(response.text)
+        # yield scrapy.Request("https://www.baidu.com", callback=self.parse)
+        # yield {
+        #     'url': response.url,
+        #     'content_length': len(response.body)
+        # }
+        # item=ScrapyspiderredisItem()
+        # item["link"]="http://127.0"
+        # item["title"]="你好"
+        # yield item
+        
