a362bac48bc8f0ba0702754f423278247bcbc7aa
shixiuhai <15256728901@163.com>
2024-06-27 21:35:03 +0800

    修改了翻译网址拼接的bug

--- spidersDemo/auction/auctionSpider.py
+++ spidersDemo/auction/auctionSpider.py
@@ -19,14 +19,15 @@ class AuctionspiderSpider(scrapy.Spider):
     def parse_artist(self,response:HtmlResponse)->Iterable[Request]:
                                                 # //*[@id="app"]/div/div[7]/div[2]/dl[2]/dt/a
         artist_detail_url_list = ["https://" + self.allowed_domains[0]+item for item in response.xpath('//*[@id="app"]/div/div[7]/div[2]/dl/dt/a/@href').extract()]
-        for artist_detail_url in  artist_detail_url_list[0:10]:
+        for artist_detail_url in  artist_detail_url_list[0:1]:
             yield PlaywrightRequest(url=artist_detail_url,callback=self.parse_artist_detail)
         next_page = response.meta["page"] + 1
-        self.logger.info(f"当前爬取的页码是{next_page}")
+        next_url=f"https://auction.artron.net/preauction/pmp?status=2&page={next_page}&orderby=2&evaluation_price=&advancedSeach=&startdate=&enddate=&rmbPriceStart=&rmbPriceEnd=&classCode=&organCode=&city=&organName=&item=&is_total=1&keyword="
+        self.logger.info(f"当前爬取的页码是{next_page-1},url地址是{response.url}")
         # 爬取到第1000页的时候结束
-        if next_page==1000:
+        if next_page>=1001:
             return 
-        yield PlaywrightRequest(url=self.start_url.replace(f"page={next_page-1}",f"page={next_page}"),callback=self.parse_artist,meta={"page":next_page},dont_filter=True)
+        yield PlaywrightRequest(url=next_url,callback=self.parse_artist,meta={"page":next_page},dont_filter=True)
 
     def parse_artist_detail(self,response:HtmlResponse)->Iterable[Request]:
         self.logger.info(f"正在解析藏品页：{response.url}")
