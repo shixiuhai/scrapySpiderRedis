2b68ac0c0850a2eb8bd8eb2729d1cace3bae5e08
shixiuhai <15256728901@163.com>
2024-06-27 20:58:38 +0800

    添加了修改过的demo

--- spidersDemo/auction/auctionSpider.py
+++ spidersDemo/auction/auctionSpider.py
@@ -11,7 +11,7 @@ class AuctionspiderSpider(scrapy.Spider):
     allowed_domains = ["auction.artron.net"]
     # start_urls = ["https://auction.artron.net"]
     start_url = "https://auction.artron.net/preauction/pmp?status=2&page=1&orderby=2&evaluation_price=&advancedSeach=&startdate=&enddate=&rmbPriceStart=&rmbPriceEnd=&classCode=&organCode=&city=&organName=&item=&is_total=1&keyword="
-    logger = Logging("baiduSpiderByPlaywright.log").get_logger() # 使用自定义日志器
+    logger = Logging("auctionSpider.log").get_logger() # 使用自定义日志器
 
     def start_requests(self) -> Iterable[Request]:
         yield PlaywrightRequest(url=self.start_url,callback=self.parse_artist,meta={"page":1},browser_type="webkit")
@@ -19,52 +19,66 @@ class AuctionspiderSpider(scrapy.Spider):
     def parse_artist(self,response:HtmlResponse)->Iterable[Request]:
                                                 # //*[@id="app"]/div/div[7]/div[2]/dl[2]/dt/a
         artist_detail_url_list = ["https://" + self.allowed_domains[0]+item for item in response.xpath('//*[@id="app"]/div/div[7]/div[2]/dl/dt/a/@href').extract()]
-        for artist_detail_url in  artist_detail_url_list:
+        for artist_detail_url in  artist_detail_url_list[0:10]:
             yield PlaywrightRequest(url=artist_detail_url,callback=self.parse_artist_detail)
         next_page = response.meta["page"] + 1
-        yield PlaywrightRequest(url=self.start_url.replace("page=1",f"page={next_page}"),callback=self.parse_artist,meta={"page":next_page})
+        # 爬取到第1000页的时候结束
+        if next_page==1000:
+            return 
+        yield PlaywrightRequest(url=self.start_url.replace(f"page={next_page-1}",f"page={next_page}"),callback=self.parse_artist,meta={"page":next_page})
 
     def parse_artist_detail(self,response:HtmlResponse)->Iterable[Request]:
-        # self.logger.info(f"正在解析藏品页：{response.url}")
-        item = AuctionSpiderItem(
+        self.logger.info(f"正在解析藏品页：{response.url}")
+        item = AuctionSpiderItem()
+        try:
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[1]/dd/text()').get() is not None:
+                item["item_title"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[1]/dd/text()').get()
             
-            # artist = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[2]/dd/a').get(),
-           
-            # creation_year = scrapy.Field()    # 创作年代
-
-            # price_sold = scrapy.Field()       # 成交价
-
-    
-            # inscription = scrapy.Field()      # 题识
-            # literature = scrapy.Field()       # 著录
-            # description = scrapy.Field()      # 拍品描述
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[1]/dl[2]/dd/span/text()').get() is not None:
+                item["artist"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[1]/dl[2]/dd/span/text()').get()
+                                        
+            if response.xpath('//*[@id="app"]//dl/dd/a/text()').extract() is not None:
+                item["category"] = response.xpath('//*[@id="app"]//dl/dd/a/text()').extract()[0].strip()
             
-        )
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[1]/dd/text()').get() is not None:
-            item["item_title"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[1]/dd/text()').get()
-        
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[2]/dd/a/text()').get() is not None:
-            item["category"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[2]/dd/a/text()').get()
-        
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[4]/dd/span/text()').get() is not None:
-            item["dimensions"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[4]/dd/span/text()').get()
-        
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[5]/dd/text()').get() is not None:
-            item["estimate"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[5]/dd/text()').get()
-        
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[1]/dd/text()').get() is not None:
-            item["auction_date"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[1]/dd/text()').get()
-        
-        # item["material"] = response.xpath("/abc").get()
-
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[2]/dd/a/text()').get() is not None:
-            item["auction_company"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[2]/dd/a/text()').get().strip()
-
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[3]/dd/a/text()').get() is not None:
-            item["auction_session"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[3]/dd/a/text()').get().strip()
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[4]/dd/span/text()').get() is not None:
+                item["dimensions"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[4]/dd/span/text()').get()
             
-        if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[4]/dd/a/text()').get() is not None:
-            item["auction_event"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[4]/dd/a/text()').get().strip()
-        if response.xpath('//*[@id="smallPic"]/@src').get() is not None: 
-            item["img_url"] = response.xpath('//*[@id="smallPic"]/@src').get()
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[5]/dd/text()').get() is not None:
+                item["estimate"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[1]/dl[5]/dd/text()').get()
+            
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[1]/dd/text()').get() is not None:
+                item["auction_date"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[2]/dl[1]/dd/text()').get()
+            
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[3]/dl/dd/span/text()').get() is not None:
+                item["material"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[3]/dl/dd/span/text()').get()
+
+            if response.xpath('//*[@id="app"]//dl/dd/a/text()').extract()  is not None:
+                item["auction_company"] = response.xpath('//*[@id="app"]//dl/dd/a/text()').extract()[1].strip()
+
+            if response.xpath('//*[@id="app"]//dl/dd/a/text()').extract() is not None:
+                item["auction_session"] = response.xpath('//*[@id="app"]//dl/dd/a/text()').extract()[2].strip()
+                            #//*[@id="app"]   /div/div[5]/div[5]/div[1]/div/div/div[2]/dl[3]/dd/a
+            if response.xpath('//*[@id="app"]//dl/dd/a/text()').extract() is not None:
+                item["auction_event"] = response.xpath('//*[@id="app"]//dl/dd/a/text()').extract()[3].strip()
+            
+            if response.xpath('//*[@id="smallPic"]/@src').get() is not None: 
+                item["img_url"] = response.xpath('//*[@id="smallPic"]/@src').get()
+                
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[2]/dd/span/text()').get() is not None:
+                item["inscription"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[2]/dd/span/text()').get()
+            
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[3]/dd/span/text()').extract() is not None:
+                item["description"] = " ".join(response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[3]/dd/span/text()').extract())
+        
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[3]/dd/span/text()').extract() is not None:
+                item["literature"] = " ".join(response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div[1]/div/div[3]/dl[3]/dd/span/text()').extract())
+
+            if response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[1]/dl[4]/dd/span/text()').get() is not None:
+                item["creation_year"] = response.xpath('//*[@id="app"]/div/div[5]/div[5]/div[1]/div/div/div[1]/dl[4]/dd/span/text()').get()
+        except Exception as error:
+            self.logger.error(error)
+            return
+            
+        
+        
         yield item
