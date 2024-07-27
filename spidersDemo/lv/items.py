694fb7ce05bd5914f21fb5bff13dbdd9760cdbf8
shixiuhai <15256728901@163.com>
2024-07-27 23:59:04 +0800

    添加了使用selenium爬取携程旅游的demo

---
+++ spidersDemo/lv/items.py
@@ -0,0 +1,29 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class ScrapyspiderredisItem(scrapy.Item):
+    """_summary_
+    table 是表名
+    =左边的字段就是mysql里表的字段
+    Args:
+        scrapy (_type_): _description_
+    """
+    # define the fields for your item here like:
+    table="demo"
+    field_one=scrapy.Field()
+    field_two=scrapy.Field()
+    
+class LvItem(scrapy.Item):
+    table="ly"
+    departure_place=scrapy.Field()
+    title=scrapy.Field()
+    price=scrapy.Field()
+    detail_url=scrapy.Field()
+    tourist_spots=scrapy.Field()
+    destination_place=scrapy.Field()
+    day_number=scrapy.Field()
+    tourist_spots_number=scrapy.Field()
